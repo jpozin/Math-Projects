e2a2ed7abfc3e1ef6dd208b2a61c5259e54c8a73
jpozin <joelpozin97@gmail.com>
2018-02-21 15:09:02 -0500

    Added tuple assignment for interval bounds

--- MonteCarloIntegration.py
+++ MonteCarloIntegration.py
@@ -7,8 +7,7 @@ def MonteCarloIntegrate(lambda_exp=lambda x: x, interval=(0, 1), n=10000):
 	interval (tuple(num, num)) is a tuple of int and/or float values; it is the interval over which the function is being integrated
 	n (int) is the number of iterations used in the Monte Carlo integration"""
 	assert type(n) == int, "n must be an integer"
-	a = interval[0]
-	b = interval[1]
+	a, b = interval[0], interval[1]
 	assert a < b, "The lower bound of the interval must be less than the upper bound of the interval"
 	random_nums = [(a + (b-a)*num) for num in rand(n)]
 	my_map = map(lambda_exp, random_nums)
