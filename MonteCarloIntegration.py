96b04ecc209b63a2e2bb3ebf0c904d92b8270c0b
jpozin <joelpozin97@gmail.com>
2018-04-30 00:16:01 -0400

    Use generator instead of list to save space

--- MonteCarloIntegration.py
+++ MonteCarloIntegration.py
@@ -9,6 +9,6 @@ def MonteCarloIntegrate(lambda_exp=lambda x: x, interval=(0, 1), n=10000):
 	assert type(n) == int, "n must be an integer"
 	a, b = interval[0], interval[1]
 	assert a < b, "The lower bound of the interval must be less than the upper bound of the interval"
-	random_nums = [(a + (b-a)*num) for num in rand(n)]
+	random_nums = ((a + (b-a)*num) for num in rand(n))
 	my_map = map(lambda_exp, random_nums)
 	return (b-a)*mean(my_map)
